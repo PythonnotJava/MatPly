652342195fee6c819d96dc894d2062f38ed82c21
PythonnotJava <2565497078@qq.com>
2024-07-12 14:41:02 +0800

    V1.0

--- MatPly_dart/functional_core.dart
+++
@@ -1,8 +0,0 @@
-// 函数式构造法
-import 'package:ffi/ffi.dart';
-
-import 'matply_api.dart';
-
-void set_round(String format) => matply__set__round(format.toNativeUtf8());
-
-void set_seed(int seed) => matply__setSeed(seed);
